cca5759bee0f71b9028bd5f02aeebcd1d968e440
Martha GOUANGA <132378582+Gheldie@users.noreply.github.com>
2024-11-20 17:13:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,21 +25,20 @@ def meteo():
 
 @app.route('/commits/')
 def commits():
-  
     url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
-    response = requests.get(url)
-    commits_data = response.json()
+    commits_data = requests.get(url).json()
 
-    commit_minutes = []
-    for commit in commits_data:
-        commit_date = commit.get('commit', {}).get('author', {}).get('date')
-        if commit_date:
-            minute = datetime.strptime(commit_date, "%Y-%m-%dT%H:%M:%SZ").strftime("%Y-%m-%d %H:%M")
-            commit_minutes.append(minute)
-    commit_counts = Counter(commit_minutes)
+    commit_counts = Counter(
+        datetime.strptime(
+            commit['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ"
+        ).strftime("%Y-%m-%d %H:%M")
+        for commit in commits_data if 'commit' in commit and 'author' in commit['commit']
+    )
+
     commit_data = [{"minute": minute, "count": count} for minute, count in commit_counts.items()]
 
-    return render_template('commits.html')
+    return render_template('commits.html', data=commit_data)
+
 
 
 @app.route("/rapport/")
